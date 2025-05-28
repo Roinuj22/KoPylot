3588ccfd3d45f73b616523d6cc99c0e86ff6d24b
Junior <165004628+Roinuj22@users.noreply.github.com>
2025-05-28 16:25:57 +0200

    Cypress MAJ

--- merge-coverage.js
+++ merge-coverage.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
-const path = require('path');
 const { createReporter } = require('istanbul-api');
 const istanbulCoverage = require('istanbul-lib-coverage');
 
-const coverageMap = istanbulCoverage.createCoverageMap({});
+const map = istanbulCoverage.createCoverageMap({});
 
-['coverage/coverage-final.json', 'coverage/cypress-coverage/coverage-final.json'].forEach(file => {
+
+['.nyc_output/out.json', 'coverage/coverage-final.json'].forEach((file) => {
     if (fs.existsSync(file)) {
-        const content = JSON.parse(fs.readFileSync(file));
-        coverageMap.merge(content);
+        const content = JSON.parse(fs.readFileSync(file, 'utf8'));
+        map.merge(content);
     }
 });
 
 const reporter = createReporter();
 reporter.addAll(['lcov', 'text']);
-reporter.write(coverageMap);
+reporter.write(map);
